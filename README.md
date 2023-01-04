93e038d064a2202050466b3b7db3075bf6e4708d
root <root@hephaestus.olympus.faerie.cx>
2023-01-04 18:56:47 +1100

    BIP39: Completed it. Look at header or README for usage First draft ;)

--- README.md
+++ README.md
@@ -6,14 +6,39 @@ This will be a tool to cover most utilities required to make a Cryptocurrency to
 
 # Plan
 
-- [ ] BIP39 - Mnemonic/entropy
+- [x] BIP39 - Mnemonic/entropy
 - [ ] BIP32 - HD Wallets
 - [ ] BIP44 - Multi-Account Herarchy
 - [ ] BIP49 - P2WPKH-nested-in-P2SH based
 - [ ] Laravel testing/integration
+
+# Usage
+Usual PHP thing to do with composer: `composer require satyr-software/cryptotool`
+## BIP39 - Mnemonic tool
+```
+use SatyrSoftware\CryptoTool\BIP39
+```
+### Entry points
+ BIP39::Mnemonic(string|array)
+ BIP39::Generate(int words)
+ BIP39::Entropy(string hextring)
+### Outputs
+ ->dumpMnemonic():array[words]
+ ->entropy : string(binary)
 
 # Ideas
 
 # Related projects
 
 - [Ian Coleman's BIP39 tool](https://github.com/iancoleman/bip39)
+
+# Credits
+
+- Based partially on code at [furqansiddiqui/bip39-mnemonic-php](https://github.com/furqansiddiqui/bip39-mnemonic-php/)
+
+Parts taken:
+- the "Words/Entropy/Generate" static functions interface
+- Wordlist finding functions (pretty generic anyway)
+
+
+
--- src/BIP39.php
+++ src/BIP39.php
@@ -1,8 +1,171 @@
 <?php
+/*
+ * BIP39 - Mnemonic library
+ *
+ * Entry point:
+ *  BIP39::Generate(wordCount)
+ *  BIP39::Mnemonic(string|array)
+ *  BIP39::Entropy($bitstring)
+ * Outputs
+ * ->entropy		// Raw binary
+ * ->dumpMnemonic()	// Dump array of mnemonic words
+ *
+ * Uses "wordCount" to compute the rest? Presetting vars is faster, but calculating on the fly allows adjustment?
+ */
+declare(strict_types=1);
+
 namespace SatyrSoftware\Cryptotool;
 
 class BIP39
 {
+	public $entropy;	// Allow access from outside just for pure query
+	private $wordList;	// Loaded upon object creation
 
+	private $language;	// just to keep track
+
+	# Accepts both raw binary, or hex(lowercase)
+	public static function Entropy(string $entropy): self
+	{
+		if (preg_match('/^[a-f0-9]{2,}$/', strtolower($entropy)))		// Only Hex characters (chances of raw string being that way is 0%)
+		{
+			self::validateEntropyHex($entropy);
+			return (new self((strlen($entropy)*3)/8))
+				->setEntropy(hex2bin($entropy));
+		}
+		if (in_array(strlen($entropy),[16,20,24,28,32]))	// Raw binary
+		{
+			return (new self((strlen($entropy)*3)/4))
+				->setEntropy($entropy);
+		}
+		throw new \Exception('Not Hex and not binary of right length');
+	}
+	# Straight generate - just need wordcount/length
+	public static function Generate(int $wordCount = 12): self
+	{
+		return (new self($wordCount))->generateEntropy();
+	}
+	# Accepts string or array
+	public static function Mnemonic($words, ?WordList $wordList = null, bool $verifyChecksum = true): self
+	{
+		if (is_string($words)) {
+			$words = explode(" ", $words);
+		}
+
+		if (!is_array($words)) {
+			throw new MnemonicException('BIP39: Mnemonic() requires an Array of words');
+		}
+		return (new self(count($words), ($wordList ? $wordlist :  'english')))
+			->resolveMnemonic($words);
+	}
+
+
+	public function __construct(int $wordCount=12,?string $language='english',?string $entropy = null)
+	{
+		if ($wordCount < 12 || $wordCount > 24) {
+			echo "BIP39: Word count of $wordCount sent in\n";
+			throw new \Exception('BIP39: Word count must be between 12-24');
+		} elseif ($wordCount % 3 !== 0) {
+			throw new \Exception('BIP39: Supported wordcount = 12,15,18,21,24');
+		}
+
+		$this->wordsCount=$wordCount;
+		$this->entropy = $entropy;
+
+		$this->language = trim($language);
+		$this->wordList = [];
+
+		$wordListFile = sprintf('%1$s%2$swordlists%2$s%3$s.txt', __DIR__, DIRECTORY_SEPARATOR, $this->language);
+		if (!file_exists($wordListFile) || !is_readable($wordListFile))
+		{
+			throw new Exception( sprintf('BIP39 wordlist for "%s" not found or is not readable', ucfirst($this->language)) );
+		}
+
+		$wordList = preg_split("/(\r\n|\n|\r)/", file_get_contents($wordListFile));
+		foreach ($wordList as $word)
+		{
+			$this->wordList[] = trim($word);
+		}
+
+		if (count($this->wordList) !== 2048)
+		{
+			throw new Exception('BIP39: Wordlist file must have exactly 2048 words');
+		}
+	}
+
+	private function setEntropy($entropy=null):self
+	{
+		$this->entropy=$entropy;
+		return $this;
+	}
+
+	private function resolveMnemonic(array $wordList):self
+	{
+		$binarystring='';
+		foreach ($wordList as $word)
+		{
+			$i=0;
+			$word=mb_strtolower($word);
+			$index=array_search($word,$this->wordList);
+			if ($index === false) { throw new \Exception('BIP39: Invalid Mnemonic words found'); }
+			$binarystring.= str_pad(decbin($index), 11, '0', STR_PAD_LEFT);
+		}
+		for ($i=0; $i< (count($wordList)*11)/8; $i++)	//  [12,15,18,21,24] words = [32,40,48,56,64] bytes
+		{
+			$this->entropy .= chr( bindec(substr($binarystring,($i*8),8)) );
+		}
+		$this->entropy=substr($this->entropy,0,-1);	// Trim the last checksum bit off
+		return $this;
+	}
+
+	private function getChecksumBits() { return strlen($this->entropy)/4; }
+
+	private function generateChecksum():string
+	{
+		$checksumChar = ord(hash("sha256",$this->entropy,true)[0]);
+		return chr( ($checksumChar>>(8-self::getChecksumBits())) << (8-self::getChecksumBits())  );	// This is appended to the end
+	}
+
+	public function dumpMnemonic():array
+	{
+		if (is_null($this->entropy)) { return []; }
+		$fullbytes=$this->entropy . self::generateChecksum();
+		$binarystring='';
+		for ($i=0; $i < strlen($fullbytes); $i++)
+		{
+			$binarystring.=str_pad( decbin(ord($fullbytes[$i])) ,8,'0',STR_PAD_LEFT);
+		}
+		if (self::getChecksumBits() !== 8 )	// if 8, it miscalcs it as 0 to 0 (trim all). at 8 bytes, it's perfectly no trim
+		{
+			$wordsplits=str_split( substr($binarystring,0,-(8-self::getChecksumBits())), 11);	// Trim off excess binary
+		}
+		else
+		{
+			$wordsplits=str_split( $binarystring, 11);	// Trim off excess binary
+		}
+		$wordlist=[];
+		foreach ($wordsplits as $wordsplit)
+		{
+#			echo "Word bits: ".$wordsplit."\n";
+			$wordlist[]=$this->wordList[bindec($wordsplit)];
+		}
+		return $wordlist;
+	}
+
+	private function generateEntropy(): self
+	{
+		$this->entropy=(random_bytes($this->entropyBits / 8));
+		return $this;
+	}
+
+	# Validation of entropy : has to be hex, and of a particular length in bits
+	private static function validateEntropyHex(string $entropy): void
+	{
+		if (!preg_match('/^[a-f0-9]{2,}$/', $entropy)) {
+			throw new \Exception('Invalid entropy (requires hexadecimal)');
+		}
+		if (!in_array(strlen($entropy), [32, 40, 48, 56, 64])) {
+			throw new \Exception('Invalid entropy length');
+		}
+	}
 }
 ?>
